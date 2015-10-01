125f309282628e4b19e3347a0724d44d14e909db
Luke Madronal <madronal@lafayette.edu>
2015-10-01 14:27:45 -0400

    initial commit.

---
+++ iFav/iFav/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iFav
+//
+//  Created by Luke Madronal on 10/1/15.
+//  Copyright © 2015 Luke Madronal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
